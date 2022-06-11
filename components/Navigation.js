084988f5c5cf7806ce7a4cfc159fd8a6e71f9a1b
Muhammad Mursyid <muhdmursyid8@gmail.com>
2022-06-11 12:58:03 +0800

    Failed Attempt at making Stack and tab navigation work together

--- components/Navigation.js
+++ components/Navigation.js
@@ -5,17 +5,23 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
+import {Ionicons} from '@expo/vector-icons';
+
 // Import Screens
 import Home from "../screens/Home";
 import Help from '../screens/Help';
 import Profile from '../screens/Profile';
+import Login from '../screens/Login';
+import Signup from '../screens/Signup';
+
+import { ScreenWidth } from './Shared';
 
 const Stack = createStackNavigator();
 
 function HomeStackScreen() {
     return (
         <Stack.Navigator>
-            <Stack.screen name="Home" component={Home} />
+            <Stack.Screen name="Home" component={Home} />
         </Stack.Navigator>
     );
 }
@@ -23,7 +29,7 @@ function HomeStackScreen() {
 function HelpStackScreen() {
     return (
         <Stack.Navigator>
-            <Stack.screen name="Help" component={Help} />
+            <Stack.Screen name="Help" component={Help} />
         </Stack.Navigator>
     );
 }
@@ -31,18 +37,47 @@ function HelpStackScreen() {
 function ProfileStackScreen() {
     return (
         <Stack.Navigator>
-            <Stack.screen name="Profile" component={Profile} />
+            <Stack.Screen name="Profile" component={Profile} />
         </Stack.Navigator>
     );
 }
 
+//bottom tab navigator
 const Tab = createBottomTabNavigator();
-
 export default function Navigation(props) {
     return (
         <NavigationContainer>
-            <Tab.Navigator>
+            <Tab.Navigator
+            screenOptions={({route}) => ({
+                headerTitle: () => <Text>Header</Text>,
+                tabBarIcon: ({focused, color, size, padding}) => {
+                    let iconName;
+                    if(route.name === 'Home') {
+                       iconName = focused ? 'home' : 'home-outline'
+                    } else if(route.name === 'Help') {
+                        iconName = focused ? 'information-circle' : 'information-circle-outline'
+                    } else if(route.name === 'Profile') {
+                        iconName = focused ? 'person' : 'person-outline'
+                    }
+
+                    return (
+                        <Ionicons 
+                            name={iconName} 
+                            size = {size} 
+                            color={color} 
+                            style={{paddingBottom: padding}} 
+                        />
+                    );
+                },
+            })}
+            tabBarOptions={{
+                labelStyle: {fontSize: 16},
+                style: {width: ScreenWidth}
+            }}
+            >
                 <Tab.Screen name='Home' component={HomeStackScreen}/>
+                <Tab.Screen name='Help' component={HelpStackScreen}/>
+                <Tab.Screen name='Profile' component={ProfileStackScreen}/>
             </Tab.Navigator>
         </NavigationContainer>
 
