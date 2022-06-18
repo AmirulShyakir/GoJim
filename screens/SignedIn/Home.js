5eee62feac6b9cf6c92f68860c3da0a7d637a340
Muhammad Mursyid <muhdmursyid8@gmail.com>
2022-06-18 20:34:30 +0800

    Added FlatList to view facilities

--- screens/SignedIn/Home.js
+++ screens/SignedIn/Home.js
@@ -1,10 +1,46 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Text, FlatList, View, StyleSheet } from 'react-native';
+
+//firebase stuff
 import { auth } from '../../firebase';
+import { db } from '../../firebase';
+import { collection, getDoc, getDocs, query, where } from 'firebase/firestore';
+
 import MainContainer from '../../components/containers/MainContainer'; 
 //texts
 import LargeText from '../../components/Texts/LargeText';
+import RegularButton from '../../components/Buttons/RegularButton';
+import { colours } from '../../components/ColourPalette';
+
+
+const {white} = colours;
+const styles = StyleSheet.create({
+    item: {
+      color: white,
+      padding: 10,
+      fontSize: 18,
+      height: 44,
+    },
+  });
+
 
 const Home = ({navigation, route}) => {
+    const [facilities, setFacilities] = useState([]);
+
+    useEffect(() => {
+        getData();
+    }, []);
+    
+    const getData = async () => {
+        const list = [];
+        const conferenceRooms = query(collection(db, "facilities"), where("type", "==", "discussionRoom"));
+        const cRoomsSnapshot = await getDocs(conferenceRooms);
+        cRoomsSnapshot.forEach(facility => {
+                list.push(facility);
+            });
+            setFacilities([...list])
+    };
+    
     useEffect(() => {
         auth.onAuthStateChanged( (user) => {
             if(user){
@@ -14,10 +50,24 @@ const Home = ({navigation, route}) => {
             }   
         })
       },[])
-
+ 
+    
+    //to print out facilities on terminal
+    const printRooms = async () => {
+        console.log("Facilities here:");
+        const conferenceRooms = query(collection(db, "facilities"), where("type", "==", "discussionRoom"));
+        const cRoomsSnapshot = await getDocs(conferenceRooms);
+        cRoomsSnapshot.forEach((doc) => {
+            console.log(doc.id, " => ", doc.data());
+        });
+    }
 
     return <MainContainer>
         <LargeText>Homescreen Stub</LargeText>
+        <FlatList
+            data={facilities}
+            renderItem={({item}) => <Text style={styles.item}>{item.id}</Text>}
+        />
     </MainContainer>
 }
 
--- screens/SignedIn/Profile.js
+++ screens/SignedIn/Profile.js
@@ -1,4 +1,5 @@
 import { auth } from '../../firebase';
+import { Image } from 'react-native';
 import MainContainer from '../../components/containers/MainContainer'; 
 //texts
 import LargeText from '../../components/Texts/LargeText';
@@ -6,6 +7,12 @@ import LargeText from '../../components/Texts/LargeText';
 const Profile = () => {
     return <MainContainer>
         <LargeText>Profile Stub</LargeText>
+        <Image 
+            style={{width: '100%', height: '50%', borderRadius: 10}}
+            source={{
+            uri:'https://firebasestorage.googleapis.com/v0/b/gojim-1d589.appspot.com/o/why01008.jpg?alt=media&token=1f35e00d-f0ab-44e0-b522-6b41b71e3dae'
+            }}
+        />
     </MainContainer>
 }
 
