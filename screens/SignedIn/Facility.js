dc53f45b22b515a70f36a00709717cfe6be4d5b3
Muhammad Mursyid <muhdmursyid8@gmail.com>
2022-06-23 01:54:00 +0800

    Edited datepicker to link to firestore

--- screens/SignedIn/Facility.js
+++ screens/SignedIn/Facility.js
@@ -1,86 +1,93 @@
-import React, { useState, useEffect } from 'react';
-import { Image, ScrollView, StyleSheet, View, Text } from 'react-native';
-import { db } from '../../firebase';
-import { doc, getDoc } from 'firebase/firestore';
+import React, { useState, useEffect } from "react";
+import { Image, ScrollView, StyleSheet, View, Text } from "react-native";
 
-import MainContainer from '../../components/containers/MainContainer'; 
+//Firestore
+import { db } from "../../firebase";
+import { doc, getDoc, setDoc, addDoc, collection } from "firebase/firestore";
 //texts
-import LargeText from '../../components/Texts/LargeText';
-
-import RegularButton from '../../components/Buttons/RegularButton';
-import RowContainer from '../../components/containers/RowContainer';
-import RegularText from '../../components/Texts/RegularText';
-import MaxCapacityContainer from '../../components/containers/MaxCapacityContainer';
-import { colours } from '../../components/ColourPalette';
+import LargeText from "../../components/Texts/LargeText";
+import RegularText from "../../components/Texts/RegularText";
+//Containers
+import MaxCapacityContainer from "../../components/containers/MaxCapacityContainer";
+import RowContainer from "../../components/containers/RowContainer";
+//Others
+import RegularButton from "../../components/Buttons/RegularButton";
+import { colours } from "../../components/ColourPalette";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
+import { async } from "@firebase/util";
 
-const {primary} = colours;
+const { primary } = colours;
 
+const Facility = ({ route }) => {
+  const facilityName = route.params.facilityName;
 
-const Facility = ({route}) => {
-    const facilityName = route.params.facilityName;
+  const [facility, setFacility] = useState({});
 
-    const [facility, setFacility] = useState({});
+  useEffect(() => {
+    getFacility();
+  }, []);
+
+  const getFacility = async () => {
+    const docRef = doc(db, "facilities", facilityName);
+    const docSnap = await getDoc(docRef);
+    const facility = docSnap.data();
+    setFacility(facility);
+  };
+
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const handleConfirm = async (date) => {
+    console.log("A date has been picked: ", date.toDateString());
+    console.log("A date has been picked: ", date);
+    const docRef = await addDoc(collection(db, "bookings"), {
+        startTime: date
+      });
+      console.log("Document written with ID: ", docRef.id);
     
-    useEffect(() => {
-        getFacility();
-    }, []);
-    
-    const getFacility = async () => {
-        const docRef = doc(db, 'facilities', facilityName);
-        const docSnap = await getDoc(docRef);
-        const facility = docSnap.data();
-        setFacility(facility);
-    }
-    
-    const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+    await setDoc(doc(db, "bookings", docRef.id), { name: 'JOE MAMA'}, {merge: true});
+    hideDatePicker();
+  };
 
-    const showDatePicker = () => {
-        setDatePickerVisibility(true);
-    };
-
-    const hideDatePicker = () => {
-        setDatePickerVisibility(false);
-    };
-
-    const handleConfirm = (date) => {
-        console.warn("A date has been picked: ", date);
-        hideDatePicker();
-    };
-
-    
-    return (
-      <ScrollView style={styles.scrollView}>
-        <Image
-          style={{ width: "100%", height: 200, borderRadius: 10 }}
-          source={{ uri: facility.imageURL }}
+  return (
+    <ScrollView style={styles.scrollView}>
+      <Image
+        style={{ width: "100%", height: 200, borderRadius: 10 }}
+        source={{ uri: facility.imageURL }}
+      />
+      <LargeText>{facilityName}</LargeText>
+      <RowContainer>
+        <RegularText>{facility.venue}</RegularText>
+        <MaxCapacityContainer>
+          <RegularText>{facility.capacity}</RegularText>
+        </MaxCapacityContainer>
+      </RowContainer>
+      <RegularText>{facility.description}</RegularText>
+      <View>
+        <RegularButton onPress={showDatePicker}>Make Booking</RegularButton>
+        <DateTimePickerModal
+          isVisible={isDatePickerVisible}
+          mode="date"
+          onConfirm={handleConfirm}
+          onCancel={hideDatePicker}
         />
-        <LargeText>{facilityName}</LargeText>
-        <RowContainer>
-          <RegularText>{facility.venue}</RegularText>
-          <MaxCapacityContainer>
-            <RegularText>{facility.capacity}</RegularText>
-          </MaxCapacityContainer>
-        </RowContainer>
-        <RegularText>{facility.description}</RegularText>
-        <View>
-          <RegularButton onPress={showDatePicker}>Make Booking</RegularButton>
-          <DateTimePickerModal
-            isVisible={isDatePickerVisible}
-            mode="date"
-            onConfirm={handleConfirm}
-            onCancel={hideDatePicker}
-          />
-        </View>
-      </ScrollView>
-    );
-}
+      </View>
+    </ScrollView>
+  );
+};
 
 const styles = StyleSheet.create({
-    scrollView: {
-      backgroundColor: primary,
-      padding: 25
-    }
+  scrollView: {
+    backgroundColor: primary,
+    padding: 25,
+  },
 });
 
 export default Facility;
