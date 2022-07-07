ba9d63aa57946b73714143cbca4aa19d7ea0622f
Amirul Shyakir <amshyaswad@gmail.com>
2022-07-08 01:18:58 +0800

    updated onboarding screen with step by step tutorial

--- screens/SignedIn/Onboarding.js
+++ screens/SignedIn/Onboarding.js
@@ -1,29 +1,45 @@
 import { Image } from 'react-native';
 import React from 'react';
-
 import Onboarding from 'react-native-onboarding-swiper';
+import { colours } from "../../components/ColourPalette";
+
+const { primary, secondary, tertiary, action } = colours;
 
 const HowItWorks = ({navigation}) => (
   <Onboarding
     onDone={() => navigation.goBack()}
+    onSkip={() => navigation.goBack()}
+    titleStyles= {{fontSize: 20, fontWeight: 'bold'}}
     pages={[
       {
-        backgroundColor: '#fff',
+        backgroundColor: primary,
         image: <Image source={require('../../assets/GojimLogo.png')} />,
-        title: 'Onboarding',
-        subtitle: 'Done with React Native Onboarding Swiper',
+        title: 'SEARCH FOR FACILITY.',
+        subtitle: 'Filter by facility type in Home',
       },
       {
         backgroundColor: '#fe6e58',
         image: <Image source={require('../../assets/GojimLogo.png')} />,
-        title: 'The Title',
-        subtitle: 'This is the subtitle that sumplements the title.',
+        title: 'JUST BOOK IT.',
+        subtitle: 'Select date and available timeslot \n And thats pretty much it',
       },
       {
         backgroundColor: '#999',
         image: <Image source={require('../../assets/GojimLogo.png')} />,
-        title: 'Triangle',
-        subtitle: "Beautiful, isn't it?",
+        title: 'MAKE IT AN EVENT.',
+        subtitle: "Okay shawty, one's company, two's a crowd and three's a party! Enter event details and publish",
+      },
+      {
+        backgroundColor: action,
+        image: <Image source={require('../../assets/GojimLogo.png')} />,
+        title: 'JOIN EVENTS.',
+        subtitle: "Fancy a quick battle? Or perhaps a study date? There's always something fresh in the Events tab. ASSC who?",
+      },
+      {
+        backgroundColor: tertiary,
+        image: <Image source={require('../../assets/GojimLogo.png')} />,
+        title: 'TRACK YOUR BOOKINGS.',
+        subtitle: "View your past and upcoming bookings in the Account tab. Psst! You can also find your favourite facilities here",
       },
     ]}
   />
