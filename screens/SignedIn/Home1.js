e74c5cd40cb9374c2bed096ae007f9270d8e106d
Muhammad Mursyid <muhdmursyid@outlook.com>
2022-07-10 21:45:52 +0800

    Added Mapview into homescreen

--- screens/SignedIn/Home1.js
+++ screens/SignedIn/Home1.js
@@ -1,62 +1,92 @@
-import { db, auth } from '../../firebase';
-import React, { useEffect, useState } from 'react';
-import {View, ImageBackground} from 'react-native'
+import { db, auth } from "../../firebase";
+import React, { useEffect, useState } from "react";
+import { View, ImageBackground, StyleSheet, Dimensions } from "react-native";
 import { collection, doc, getDoc, getDocs, query } from "firebase/firestore";
-import SignedInContainer from '../../components/containers/SignedInContainer';
-import SelectList from 'react-native-dropdown-select-list';
+import SignedInContainer from "../../components/containers/SignedInContainer";
+import SelectList from "react-native-dropdown-select-list";
+import MapView, { Marker } from "react-native-maps";
+
 //texts
-import LargeText from '../../components/Texts/LargeText';
-import RegularButton from '../../components/Buttons/RegularButton';
-import { colours} from '../../components/ColourPalette';
-import RowContainer from '../../components/containers/RowContainer';
-import SearchButton from '../../components/Buttons/SearchButton';
+import LargeText from "../../components/Texts/LargeText";
+import RegularButton from "../../components/Buttons/RegularButton";
+import { colours } from "../../components/ColourPalette";
+import RowContainer from "../../components/containers/RowContainer";
+import SearchButton from "../../components/Buttons/SearchButton";
 
-const {white, secondary, primary} = colours;
+const { white, secondary, primary } = colours;
 
-const Home1 = ({navigation, route}) => {
+const Home1 = ({ navigation, route }) => {
   useEffect(() => {
-    auth.onAuthStateChanged( (user) => {
-        if(user){
-      
-        } else {
-            route.params.authenticate(false);
-        }   
-    })
-  },[])
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+      } else {
+        route.params.authenticate(false);
+      }
+    });
+  }, []);
 
   const pressFacil = () => {
     console.log(selected);
-    navigation.navigate('HomeScreen', {facilityType: selected});
-  }
+    navigation.navigate("HomeScreen", { facilityType: selected });
+  };
   const [selected, setSelected] = useState("");
   const data = [
-    {key:'Discussion Rooms',value:'Discussion Rooms'},
-    {key:'Event Spaces',value:'Event Spaces'},
-    {key:'Sports Courts',value:'Sports Courts'},
-    {key:'Studios',value:'Studios'},
+    { key: "Discussion Rooms", value: "Discussion Rooms" },
+    { key: "Event Spaces", value: "Event Spaces" },
+    { key: "Sports Courts", value: "Sports Courts" },
+    { key: "Studios", value: "Studios" },
   ];
-  
+
   return (
-    <ImageBackground 
-      source={{uri: 'https://firebasestorage.googleapis.com/v0/b/gojim-1d589.appspot.com/o/dark_mode_map.jpg?alt=media&token=af87d869-f225-413e-9b9b-7c9b02155255'}}
-      style={{height: '100%'}}
-    >
-      <SelectList
-        placeholder='Facility Types'
-        setSelected={setSelected} data={data} 
-        boxStyles={{
-          backgroundColor: white,
-          marginRight: 150,
+    <View>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 1.2975006718982152,
+          longitude: 103.7766084407046,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008,
         }}
-        dropdownStyles={{
-          backgroundColor: white,
-          marginRight: 150,
-        }}
-      />
-      <RegularButton onPress={pressFacil}>Search</RegularButton>
-      <LargeText>This map is a stub</LargeText>
-    </ImageBackground>
-  )
-}
+      >
+        <Marker
+          coordinate={{
+            latitude: 1.2975006718982152,
+            longitude: 103.7766084407046,
+          }}
+          title="NUS"
+        />
+      </MapView>
+      <View style={{ padding: 5, position: "absolute", width: "100%" }}>
+        <SelectList
+          placeholder="Facility Types"
+          setSelected={setSelected}
+          data={data}
+          boxStyles={{
+            backgroundColor: white,
+            marginRight: 150,
+          }}
+          dropdownStyles={{
+            backgroundColor: white,
+            marginRight: 150,
+          }}
+        />
+        <RegularButton onPress={pressFacil}>Search</RegularButton>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
 
 export default Home1;
