b85833498cbed82fe9d97343d28af83896cfbbb0
Amirul Shyakir <amshyaswad@gmail.com>
2022-07-24 21:21:18 +0800

    added test for onboarding

--- screens/tests/Onboarding.test.js
+++ screens/tests/Onboarding.test.js
@@ -1,8 +1,30 @@
 import React from "react";
 import renderer from 'react-test-renderer';
 import Onboarding from "../SignedIn/Onboarding";
+import { render, fireEvent } from "@testing-library/react-native";
 
 test("Onboarding Snapshot test", () => {
     const tree = renderer.create(<Onboarding />).toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+test("Renders first page but not the next few pages", () => {
+  const { queryAllByText, getByText, getByTestId, queryAllByTestId }= render(<Onboarding />);
+  expect(queryAllByText("BOOKINGS MADE SIMPLE.").length).toBe(1);
+  getByTestId("page1");
+  //other pages
+  expect(queryAllByText("SEARCH FOR FACILITY.").length).toBe(0);
+  expect(queryAllByTestId("page2")).toBeNull;
+
+  expect(queryAllByText("JUST BOOK IT.").length).toBe(0);
+  expect(queryAllByTestId("page3")).toBeNull;
+
+  expect(queryAllByText("MAKE IT AN EVENT.").length).toBe(0);
+  expect(queryAllByTestId("page4")).toBeNull;
+
+  expect(queryAllByText("JOIN EVENTS.").length).toBe(0);
+  expect(queryAllByTestId("page5")).toBeNull;
+
+  expect(queryAllByText("TRACK YOUR BOOKINGS.").length).toBe(0);
+  expect(queryAllByTestId("page6")).toBeNull;
+})
